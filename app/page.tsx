b3eea688e1c9c411c154da0f8b9d39f619fc26ba
badmood111 <petrzela.adam@seznam.cz>
2023-05-18 06:19:31 +0200

    change whitespace

--- app/page.tsx
+++ app/page.tsx
@@ -12,55 +12,57 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <div className="mt-16 flex h-1/6 items-center justify-center text-3xl font-light">
-        <span className="text-slate-500">{"< "}</span>
-        <button
-          onClick={() => {
-            Cookies.set('visibleState', "true");
-            setVisible(true);
-          }}
-        >
-          &nbsp;who?&nbsp;
-        </button>
-        <span className="text-slate-500">{" />"}</span>
-      </div>
+    <main className="flex h-5/6 flex-col justify-between">
+      {/* Who section */}
+      <section>
+        <div className="flex h-1/6 items-center justify-center text-3xl font-light">
+          <span className="text-slate-500">{"< "}</span>
+          <button
+            onClick={() => {
+              Cookies.set("visibleState", "true");
+              setVisible(true);
+            }}
+          >
+            &nbsp;who?&nbsp;
+          </button>
+          <span className="text-slate-500">{" />"}</span>
+        </div>
 
-      <div
-        className={
-          "mt-10 flex h-2/6 flex-col items-center justify-center gap-[1px] text-3xl font-light opacity-0" +
-          (visible ? "opacity-100" : "opacity-0") +
-          " transition-all duration-700"
-        }
-      >
-        <Link
-          href="/dev"
-          className="mx-auto w-1/4 border border-black p-4 text-center transition-all duration-300 hover:border-slate-500 active:bg-slate-800"
+        <div
+          className={
+            "mt-20 flex h-2/6 flex-col items-center justify-center gap-[1px] text-3xl font-light opacity-0" +
+            (visible ? "opacity-100" : "opacity-0") +
+            " transition-all duration-700"
+          }
         >
-          Software developer
-        </Link>
-        <Link
-          href="/books"
-          className="mx-auto w-1/4 border border-black p-4 text-center transition-all duration-300 hover:border-slate-500 active:bg-slate-800"
-        >
-          Reader
-        </Link>
-        <Link
-          href="dreams"
-          className="mx-auto w-1/4 border border-black p-4 text-center transition-all duration-300 hover:border-slate-500 active:bg-slate-800"
-        >
-          Dreamer
-        </Link>
-      </div>
+          <Link
+            href="/dev"
+            className="mx-auto w-1/4 border border-black p-4 text-center transition-all duration-300 hover:border-slate-500 active:bg-slate-800"
+          >
+            Software developer
+          </Link>
+          <Link
+            href="/books"
+            className="mx-auto w-1/4 border border-black p-4 text-center transition-all duration-300 hover:border-slate-500 active:bg-slate-800"
+          >
+            Reader
+          </Link>
+          <Link
+            href="dreams"
+            className="mx-auto w-1/4 border border-black p-4 text-center transition-all duration-300 hover:border-slate-500 active:bg-slate-800"
+          >
+            Dreamer
+          </Link>
+        </div>
+      </section>
 
-      <footer className="">
-        <p className="mt-48 flex items-center justify-center p-6 text-2xl tracking-widest">
-          <span className="text-slate-500">{"<"}</span>
-          <Link href={"/contact"}>&nbsp;Contact&nbsp;</Link>
-          <span className="text-slate-500">{"/>"}</span>
-        </p>
-      </footer>
-    </div>
+      {/* Contact footer */}
+      <p className="flex items-center justify-center p-6 text-2xl tracking-widest">
+        <span className="text-slate-500">{"<"}</span>
+        <Link href={"/contact"}>&nbsp;Contact&nbsp;</Link>
+        <span className="text-slate-500">{"/>"}</span>
+      </p>
+    </main>
   );
 };
 
